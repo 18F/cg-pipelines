7e4134ac85219b985c56a2c303e4c3e99975902c
Adrian Webb <adrian.webb@gsa.gov>
2015-11-16 18:23:17 -0500

    Adding scaffolding register and destroy scripts.

---
+++ destroy.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# TODO: Complete this destroy script
+#
+# The destroy.sh script loops through all of the defined pipelines and either
+# calls the destroy.sh script in each pipeline directory or runs a default
+# fly destroy-pipeline command if no destroy.sh script is found for the pipeline.
---
+++ register.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# TODO: Complete this registration script
+#
+# The register.sh script loops through all of the defined pipelines and either
+# calls the register.sh script in each pipeline directory or runs a default
+# fly set-pipeline command is no register.sh script is found for the pipeline.
