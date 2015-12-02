d662a5b1b95b1df8f0fc8d51dceb590bdbe98734
M. Adam Kendall <michael.kendall@gsa.gov>
2015-12-02 17:17:23 -0500

    Update tests so that grep match can happen at the start of the version output

--- docker/lib/test_command.sh
+++ docker/lib/test_command.sh
@@ -6,9 +6,9 @@ function test_command {
   local name=$1
   local version=$2
   local version_options="${3:---version}"
-  
+
   echo -n "it should expose the $name command... "
-   $name $version_options 2>&1 | grep " $version" > /dev/null
+   $name $version_options 2>&1 | grep -E "( |^)$version" > /dev/null
   [ "$?" -ne 0 ] && echo fail && exit 1
-  echo pass  
+  echo pass
 }
